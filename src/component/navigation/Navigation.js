b27c412c2c0058dbf3227aabdfc5890095bed02c
diegoPanino <tieso.diego@gmail.com>
2020-06-08 10:36:18 +0200

    1st Upoload

---
+++ src/component/navigation/Navigation.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Navigation = () =>{
+	return(
+		<h1>ciao</h1>
+		)
+}
---
+++ src/container/App.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import {Navigation} from '../component/navigation/Navigation';
+
+import '../css/App.css';
+
+export default class App extends Component{
+  render(){
+    return(
+      <div>
+        <Navigation />
+      </div>
+      )
+  }
+}
